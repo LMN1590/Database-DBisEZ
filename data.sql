b0854f029998a8784a5e794a0caa5fe6b7f0e972
AD72001 <80466872+AD72001@users.noreply.github.com>
2022-11-19 16:03:02 +0700

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,11 @@
+-- tai khoan
+INSERT INTO taikhoan VALUES(1, 'amazonjuicy', 'dugjhr88', '0234569874', 'chandanhvl@gmail.com');
+INSERT INTO taikhoan VALUES(2, 'weirdofoxxx', 'foxxxxx', '0168974512', 'haicham3que@gmail.com');
+INSERT INTO taikhoan VALUES(3, 'bamboohoo', 'assisgood1', '0785642135', 'boohuhu@gmail.com');
+INSERT INTO taikhoan VALUES(4, 'goldtdawg21', 'CXxdkixl', '0580685080', 'golddawg22@gmail.com');
+INSERT INTO taikhoan VALUES(5, 'siomaithuy', 'eJfAC1tT', '0553301100', 'crackerhero@gmail.com');
+INSERT INTO taikhoan VALUES(6, 'sktno1', 'pWgH00tQ', '048102565', 'doubled@gmail.com');
+INSERT INTO taikhoan VALUES(7, 'boomerfaker', 'N1ri2TLD', '0666889572', 'canhcutno1@gmail.com');
+INSERT INTO taikhoan VALUES(8, 'zoomer234', 'iDAqVGUd', '0229031171', 'trustmebro@gmail.com');
+INSERT INTO taikhoan VALUES(9, 'tuanITpro', 'hmA9OFCW', '0739521401', 'tuanvjpit@gmail.com');
+INSERT INTO taikhoan VALUES(10, 'ADPKsieu', 'XskUyJO9', '0944327626', 'adpkno1xathu@gmail.com');
