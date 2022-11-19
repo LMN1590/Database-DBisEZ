83cf7a22defda365544a802d0b244efc55a38fa8
AD72001 <80466872+AD72001@users.noreply.github.com>
2022-11-19 16:21:16 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -10,6 +10,7 @@ INSERT INTO taikhoan VALUES(8, 'zoomer234', 'iDAqVGUd', '0229031171', 'trustmebr
 INSERT INTO taikhoan VALUES(9, 'tuanITpro', 'hmA9OFCW', '0739521401', 'tuanvjpit@gmail.com');
 INSERT INTO taikhoan VALUES(10, 'ADPKsieu', 'XskUyJO9', '0944327626', 'adpkno1xathu@gmail.com');
 
+-- admin
 INSERT INTO admin VALUES (1, 'Chan', 'Danh');
 INSERT INTO admin VALUES (2, 'Hai', 'Que');
 INSERT INTO admin VALUES (3, 'Bu', 'Hu');
@@ -20,3 +21,15 @@ INSERT INTO admin VALUES (7, 'Lee', 'Sanghyeok');
 INSERT INTO admin VALUES (8, 'Tre', 'Nghe');
 INSERT INTO admin VALUES (9, 'Nguyen', 'Tuan');
 INSERT INTO admin VALUES (10, 'Pham', 'Duy');
+
+--khuyen mai chung
+INSERT INTO khuyenmai_chung VALUES (1, 1200000, 'Giảm tối đa 150k đơn tối thiểu 1200k', 'COSLUX11', '20/11/2022', '30/11/2022', 100, 81, 1);
+INSERT INTO khuyenmai_chung VALUES (2, 50, 'Giảm tối đa 50k đơn tối thiểu 50k', 'VISA50', '8/11/2022', '8/12/2022', 200, 54, 2);
+INSERT INTO khuyenmai_chung VALUES (3, 200000, 'Giảm 40% cho đơn hàng từ 200k', 'GF40200', '4/10/2022', '4/12/2022', 500, 2, 3);
+INSERT INTO khuyenmai_chung VALUES (4, 300000, 'Giảm 75k cho đơn hàng từ 300k', 'SL75', '1/10/2022', '30/11/2022', 400, 43, 4);
+INSERT INTO khuyenmai_chung VALUES (5, 20000, 'Giảm giá tối đa 10k cho đơn từ 20k tri ân 20/11', 'TD2022', '16/11/2022', '21/11/2022', 300, 66, 5);
+INSERT INTO khuyenmai_chung VALUES (6, 200000, 'Giảm 30k cho đơn hàng từ 200k', 'FREE200', '4/11/2022', '18/12/2022', 50, 21, 6);
+INSERT INTO khuyenmai_chung VALUES (7, 0, 'Khuyến mãi 50% khi mua trà sữa', '50TRASUA', '17/11/2022', '17/12/2022', 200, 102, 7);
+INSERT INTO khuyenmai_chung VALUES (8, 75000, 'Khuyến mãi 20% cho đơn từ 75k', 'DG7520', '15/11/2022', '30/11/2022', 100, 32, 8);
+INSERT INTO khuyenmai_chung VALUES (9, 0, 'Mã giảm 20k cho khách hàng sử dụng GrabFood', 'MCGRAB', '10/10/2022', '30/11/2022', 70, 5, 9);
+INSERT INTO khuyenmai_chung VALUES (10, 300000, 'Giảm 50% cho đơn hàng từ 300k', 'HOT300', '11/11/2022', '30/11/2022', 50, 0, 10);
