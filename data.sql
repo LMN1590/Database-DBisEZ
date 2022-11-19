8d5b68c8db614661816ab80ff85758c860770bcf
nhantran18 <80414510+nhantran18@users.noreply.github.com>
2022-11-19 18:17:36 +0700

    thêm data công ty vận chuyển

--- data.sql
+++ data.sql
@@ -33,3 +33,10 @@ INSERT INTO khuyenmai_chung VALUES (7, 0, 'Khuyến mãi 50% khi mua trà sữa'
 INSERT INTO khuyenmai_chung VALUES (8, 75000, 'Khuyến mãi 20% cho đơn từ 75k', 'DG7520', '15/11/2022', '30/11/2022', 100, 32, 8);
 INSERT INTO khuyenmai_chung VALUES (9, 0, 'Mã giảm 20k cho khách hàng sử dụng GrabFood', 'MCGRAB', '10/10/2022', '30/11/2022', 70, 5, 9);
 INSERT INTO khuyenmai_chung VALUES (10, 300000, 'Giảm 50% cho đơn hàng từ 300k', 'HOT300', '11/11/2022', '30/11/2022', 50, 0, 10);
+
+--cong ty van chuyen
+INSERT INTO congtyvanchuyen VALUES ('congtyvanchuyen1', '1' , 'GrabFood', '0902348144', 'grabfood@grab.com', 'Tòa nhà Mapletree Business Center, 1060 Nguyễn Văn Linh, phường Tân Phong, quận 7, TPHCM');
+INSERT INTO congtyvanchuyen VALUES ('congtyvanchuyen2', '2' , 'beFood', '1900232345' , 'hotro@be.com.vn', 'Tầng 16, tòa nhà Saigon Tower, 29 Lê Duẩn, phường Bến Nghé, quận 1, TPHCM');
+INSERT INTO congtyvanchuyen VALUES ('congtyvanchuyen3', '3' , 'Baemin', '19003458' , 'help@baemin.vn', 'Tầng 4 và Tầng 5, Friendship Tower, 31 Lê Duẩn, phường Bến Nghé, quận 1, TPHCM');
+INSERT INTO congtyvanchuyen VALUES ('congtyvanchuyen4', '4' , 'ShopeeFood', '19002042' , 'support@shopeefood.vn', 'Lầu G, tòa nhà Jabes 1, 244 Cống Quỳnh, phường Phạm Ngũ Lão, quận 1, TPHCM');
+INSERT INTO congtyvanchuyen VALUES ('congtyvanchuyen5', '5' , 'Loship', '1900638058' , 'hotroloship@lozi.vn', 'Tòa nhà Lữ Gia, Tầng 4 - 70 Lữ Gia, phường 15, quận 11, TPHCM');
