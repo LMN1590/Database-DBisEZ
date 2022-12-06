47cb99cf19237069e63505f583c6d4f3d9e40e11
nhantran18 <80414510+nhantran18@users.noreply.github.com>
2022-12-06 22:06:24 +0700

    Update tính tiền đơn hàng có khuyến mãi

--- function.sql
+++ function.sql
@@ -29,3 +29,63 @@ DELIMITER ;
 
 SELECT TONGSOKHUYENMAI(40);
 
+
+-- Viết Function tính tiền đơn hàng có khuyến mãi
+drop function if exists TONGTIENDONHANG; 
+DELIMITER $$ 
+CREATE FUNCTION TONGTIENDONHANG(donhang_ID INT) RETURNS INT deterministic
+BEGIN
+	DECLARE tongtien INT default 0;
+    DECLARE giagoc INT default 0;
+    DECLARE x INT default 0;
+    DECLARE y INT default 0;
+    DECLARE z INT default 0;
+    DECLARE phan_tram_giam INT default 0;
+	DECLARE tien_giam_toi_da INT default 0;
+    declare t int default 0;
+    SET x = (SELECT COUNT(ID) from donhang where ID = donhang_ID);
+    IF(x = 0) THEN
+		RETURN -1;
+	ELSE
+		SET tongtien =  (SELECT SUM(Soluongmon * Giamua) AS TOTAL
+		FROM donhang JOIN noidung_donhang ON donhang.ID = noidung_donhang.ID_donhang
+		WHERE donhang.ID = donhang_ID
+		GROUP BY ID
+		);
+        SET giagoc = tongtien;
+        -- Trừ tất cả các mã giảm tiền
+        -- ĐANG LÀM Ở ĐÂY :))))))))))))
+        SET y = (SELECT SUM(Sotiengiam)
+        FROM(
+        SELECT ID_makhuyenmai, Sotiengiam
+        FROM apdung_khuyenmai a JOIN khuyenmaigiamtien k ON a.ID_makhuyenmai = k.IDcuama
+		WHERE ID_donhang = donhang_ID) AS BANG_GIAM_TIEN_DOI_VOI_DONHANG_ID);
+        IF(y IS NOT NULL) THEN
+			SET tongtien = tongtien - y;
+		END IF;
+        -- Trừ tất cả các mã giảm phần trăm
+        SET phan_tram_giam = (SELECT Phantramgiam
+        FROM apdung_khuyenmai a JOIN khuyenmaiphantram k ON a.ID_makhuyenmai = k.IDcuama
+		WHERE ID_donhang = donhang_ID);
+        SET tien_giam_toi_da = (SELECT Tiengiamtoida
+        FROM apdung_khuyenmai a JOIN khuyenmaiphantram k ON a.ID_makhuyenmai = k.IDcuama
+		WHERE ID_donhang = donhang_ID);
+        IF(phan_tram_giam IS NOT NULL) THEN
+			SET t = phan_tram_giam*giagoc/100;
+			IF(t > tien_giam_toi_da) THEN
+				SET tongtien = tongtien - tien_giam_toi_da;
+			ELSE
+				SET tongtien = tongtien - t;
+			END IF;
+		END IF;
+		-- Lấy từng ID mã khuyến mãi trong bảng T1 
+        -- Dùng IF-ELSE và count(ID) để xét khuyến mãi đó thuộc bảng nào
+        -- Nếu thuộc bảng khuyến mãi giảm tiền -> khiểm tra tối thiểu -> trừ vào biến tổng tiền
+        -- Nếu thuộc bẳng khuyến mãi giảm theo phần trăm -> trừ theo giá gốc
+        
+		RETURN tongtien;
+	END IF;
+END $$
+DELIMITER ;
+
+SELECT TONGTIENDONHANG(12);
